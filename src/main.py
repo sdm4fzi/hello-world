e670fafdc5803c46e3b9a8c998ecbd164d7af9da
Christian Berendt <berendt@23technologies.cloud>
2022-05-02 12:23:18 +0200

    Use methods
    Signed-off-by: Christian Berendt <berendt@23technologies.cloud>

--- src/main.py
+++ src/main.py
@@ -3,15 +3,28 @@ import time
 
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 
+
+def connect(modbus_port="/dev/ttyAMA2", modbus_baudrate=19200):
+    client = ModbusClient(method="rtu", port=modbus_port, stopbits=1, bytesize=8, parity='E', baudrate=modbus_baudrate)
+    client.connect()
+    return client
+
+
+def close(client):
+    client.close()
+
+
+def stop(client):
+    client.write_coil(0x00, 0, unit=0x01)
+    client.write_coil(0x01, 0, unit=0x01)
+
+
 modbus_port = os.environ.get("MODBUS_PORT", "/dev/ttyAMA2")
 modbus_baudrate = int(os.environ.get("MODBUS_BAUDRATE", 19200))
 
-client = ModbusClient(method="rtu", port=modbus_port, stopbits=1, bytesize=8, parity='E', baudrate=modbus_baudrate)
-connection = client.connect()
+client = connect(modbus_port, modbus_baudrate)
 
-# stop
-client.write_coil(0x00, 0, unit=0x01)
-client.write_coil(0x01, 0, unit=0x01)
+stop(client)
 
 # reset (move left)
 result1 = client.read_discrete_inputs(0x00, count=1, unit=0x02)
@@ -43,7 +56,5 @@ try:
             time.sleep(1)
 
 except KeyboardInterrupt:
-    # stop
-    client.write_coil(0x00, 0, unit=0x01)
-    client.write_coil(0x01, 0, unit=0x01)
-    client.close()
+    stop(client)
+    close(client)
